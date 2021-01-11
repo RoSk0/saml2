feb003d4837408dd8f742ee7230b4b0b61c0bea0
Tim van Dijen <tvdijen@gmail.com>
2021-01-11 13:07:03 +0100

    Generate assertion from scratch

--- bin/entityattributes.php
+++ bin/entityattributes.php
@@ -6,8 +6,12 @@ require_once(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
 use SimpleSAML\SAML2\Constants;
 use SimpleSAML\SAML2\XML\mdattr\EntityAttributes;
 use SimpleSAML\SAML2\XML\saml\Attribute;
+use SimpleSAML\SAML2\XML\saml\AttributeStatement;
 use SimpleSAML\SAML2\XML\saml\AttributeValue;
 use SimpleSAML\SAML2\XML\saml\Assertion;
+use SimpleSAML\SAML2\XML\saml\Issuer;
+use SimpleSAML\SAML2\XML\saml\NameID;
+use SimpleSAML\SAML2\XML\saml\Subject;
 use SimpleSAML\XML\DOMDocumentFactory;
 use SimpleSAML\XMLSecurity\TestUtils\PEMCertificatesMock;
 use SimpleSAML\XMLSecurity\XMLSecurityKey;
@@ -34,15 +38,24 @@ $attribute2 = new Attribute(
     ]
 );
 
-$assertion = DOMDocumentFactory::fromFile('../tests/resources/xml/assertions/unsignedassertion_only_attributestatement.xml');
-$unsignedAssertion = Assertion::fromXML($assertion->documentElement);
+$unsignedAssertion = new Assertion(
+    new Issuer('Provider'),
+    '_93af655219464fb403b34436cfb0c5cb1d9a5502',
+    1457707995,
+    new Subject(
+        new NameID('s00000000:123456789', null, null, Constants::NAMEID_PERSISTENT)
+    ),
+    null, // Conditions
+    [
+        new AttributeStatement(
+            [$attribute1]
+        )
+    ]
+);
 
 $privateKey = PEMCertificatesMock::getPrivateKey(XMLSecurityKey::RSA_SHA256, PEMCertificatesMock::SELFSIGNED_PRIVATE_KEY);
 $unsignedAssertion->setSigningKey($privateKey);
 $unsignedAssertion->setCertificates([PEMCertificatesMock::getPlainPublicKey(PEMCertificatesMock::SELFSIGNED_PUBLIC_KEY)]);
 
-$signedAssertion = $unsignedAssertion->toXML();
-echo 'PRE:  ' . $signedAssertion->ownerDocument->saveXML();
-
-$ea = new EntityAttributes([$attribute1, Assertion::fromXML($signedAssertion), $attribute2]);
-echo 'POST:  ' . strval($ea->toXML()->ownerDocument->saveXML());
+$ea = new EntityAttributes([$attribute1, $unsignedAssertion, $attribute2]);
+echo strval($ea->toXML()->ownerDocument->saveXML());
